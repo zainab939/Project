f14eeb56021d2ee692bcb6bbe39a8f700f7e9b29
AdamuBilal <your-email@example.com>
2026-05-13 03:29:42 +0100

    iiniitialize course track repo

---
+++ Main/js/dashboard.js
@@ -0,0 +1,15 @@
+import { createIcons, Menu, ArrowRight, Globe } from 'lucide';
+
+createIcons({
+  icons: {
+    bookOpen,
+    Menu,
+    ArrowRight,
+    Globe
+  }
+});
+
+const addCourses = document.querySelector('add-courses');
+addCourses.addEventListener('click', () => (
+  
+);
